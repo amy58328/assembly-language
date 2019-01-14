40069a9fc61f7f7e1018eacf66ba35ebba57827e
amy58328 <amy58328@gmail.com>
2019-01-14 14:11:09 +0800

    test

--- test.asm
+++ test.asm
@@ -1,70 +1,32 @@
 .386
-
 Include \masm32\include\Irvine32.inc
 Includelib \masm32\lib\Irvine32.lib 
 includelib \masm32\lib\Kernel32.lib
 includelib \masm32\lib\User32.lib
 
+
 .data
+	tableb	byte 10,20,30,40,50
+	rowsize = ($ - tableb)
+			byte 60,70,80,90,100
+			byte 110,120,130,140,150
 
-	lon dword ?
-	string1 byte 80 dup(?)
-	ans dword 0
-
-	find proto,
-	use: ptr byte
-	
+	; row byte 1
+	; col byte 1
 .code
 main proc
-	
-	mov ecx,	lengthof string1
-	mov edx,	offset	string1
-	call readstring
-	mov lon,	eax
+	mov eax,1
+	mov ebx,5
+	mul ebx
 
-	invoke find,addr string1
+	mov ebx, offset tableb
+	add ebx,eax
 
-	call printout
+	mov esi,	1
+	mov eax,0
+	mov al,[ebx + esi]
+	call writeint
 
-	exit
-main endp 
-
-printout proc
-
-	mov edx,	offset string1
-	call writestring
-	call crlf
-	
-	mov eax, ans
-	call writedec
-	call crlf
-
-	ret 
-printout endp
-
-find proc,
-	use : ptr byte
-
-	mov ecx,	lon
-	mov eax,	0
-
-	mov bl, '\'
-	mov esi,	use
-
-L1:
-	cmp [esi],	bl
-	je	same
-	jmp other 
-
-	same:
-		inc ans
-		mov al," "
-		mov [esi], al
-	other:
-		inc esi
-
-	loop L1
-	ret
-find endp
-	
+exit
+main endp
 end main
